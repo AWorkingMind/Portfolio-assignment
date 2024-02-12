c798b756d84d63a704f574a6fb6ee8e1cbda810a
XboXChristian <Chrtobias4780@gmail.com>
2024-02-12 13:57:57 +0100

    Showcase fixes and text

--- ClickListner.js
+++ ClickListner.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('.card').forEach(card => {
+document.querySelectorAll('.card, .card-sm').forEach(card => {
     card.addEventListener('click', function() {
         // Toggle active class on click
         this.classList.toggle('active');
 
         // If you want only one card to expand at a time, you can add this code to close other cards
-        document.querySelectorAll('.card').forEach(otherCard => {
+        document.querySelectorAll('.card, .card-sm').forEach(otherCard => {
             if (otherCard !== this) {
                 otherCard.classList.remove('active');
             }
